06d43aa113f1e7bac32b64d0f4180a2725504722
Guilherme Oliveira <oliveiragl615@gmail.com>
2023-04-25 21:23:23 -0300

    Update exe03.js

--- JavaScript/exe03.js
+++ JavaScript/exe03.js
@@ -2,32 +2,34 @@ const transformDegree = (degree) => {
     
     const ifCelsius = degree.toUpperCase().includes('C') 
     const ifFahrenheit = degree.toUpperCase().includes('F')
-
-    const newFahrenheit = Number(degree.toUpperCase().replace('F', ''))
-    const newCelsius = Number(degree.toUpperCase().replace('C', ''))
     
-    const toFahrenheit = (c) => c * 9 / 5 + 32
-    const toCelsius = (f) => (f - 32) * 5 / 9
+    let newDegree = Number(degree.toUpperCase().replace('C', ''))
+    let formula = (c) => c * 9 / 5 + 32 
     
-    if (!ifCelsius && !ifFahrenheit) {
-        
+    //Fluxo de Erro
+    if (!ifCelsius && !ifFahrenheit) { 
         throw new Error('Grau não identificado')
+    } 
+    
+    //Fluxos alternativos C -> F ou F-> C
+    if (ifCelsius) {
+
+        return formula(newDegree) + 'F'     // C -> F
 
     } else if (ifFahrenheit) {
 
-        return toCelsius(newFahrenheit) + 'C'
-
-    } else if (ifCelsius) {
-
-        return toFahrenheit(newCelsius) + 'F'
-
+        newDegree = Number(degree.toUpperCase().replace('F', ''))
+        formula = (f) => (f - 32) * 5 / 9
+        
+        return formula(newDegree) + 'C'   //  F -> C
     }
+    
 
 }
 
 try {
-    console.log(transformDegree('104F'))
-    console.log(transformDegree('40C'));
+    console.log(transformDegree('50F'))
+    console.log(transformDegree('10C'));
 } catch (error) {
     console.log(error)
 }
